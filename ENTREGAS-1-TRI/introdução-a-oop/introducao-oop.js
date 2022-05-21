19321eedfee177ad9a1c3110489020c295934bbf
Airamzin <104568587+airamzin006@users.noreply.github.com>
2022-05-21 18:36:45 -0300

    Add files via upload

---
+++ ENTREGAS-1-TRI/introdução-a-oop/introducao-oop.js
@@ -0,0 +1,30 @@
+import * as readline from 'readline'
+import { stdin as input, stdout as output } from 'process'
+const rl = readline.createInterface({ input, output })
+const question = q => new Promise((rs, rj) => rl.question(q + ' ', a => rs(a)))
+
+let soma = 0
+let count = 0
+let media = 0
+
+while (true) {
+    let strNota = await question(`Digite o valor da ${count + 1}° nota ou N/n para cacular a média entre os valores inseridos:`)
+    
+    if (strNota.toUpperCase() == 'N') {
+        break
+    }
+    
+    let nota = parseFloat(strNota)
+    
+    if (isNaN(nota) || nota < 0 || nota >10) {
+        console.log("[ERRO] insira um numero real que seja maior ou igual a 0 e igual ou menor a 10.")
+        continue
+    }
+
+    soma = soma + nota
+    count++
+}
+
+media = soma / count
+console.log(`[ RESULTADO ] A média de todos os valores informados é`, media)
+process.exit()
