a008c9796611de2d2ee89105cb9fb75c72cc6408
Your Name <you@example>
2022-05-26 10:02:26 -0300

    aula 1

--- breakout-atari-oop/main.js
+++ breakout-atari-oop/main.js
@@ -1,18 +1,34 @@
 const canvas = document.createElement("canvas")
 const context = canvas.getContext("2d")
 
-canvas.width = 800
+canvas.width = 1000
 canvas.height = 600
 
-let y = 0
-let barSize = 100
-let  directionY = 1
+let X = 0
+let barWidth = 100
+let barHeight = 20
+let directionY = 1
+
+document.addEventListener("keydown", ev => {
+    if (!ev.repeat && ev.code == "KeyD"){
+        return directionX = 2
+    }
+    if (!ev.repeat && ev.code == "KeyA"){
+        return directionX = -2
+    }
+})
+
+document.addEventListener("keyup", ev=>{
+    if (ev.code == "KeyD" || ev.code == "KeyA"){
+        directionX = 0
+    }
+})
 
 function mainLoop(){
     context.clearRect( 0, 0 , canvas.width , canvas.height )
-    context.fillRect( 10, y , 20 , 100 )
-    y += directionY
-    if (y <=0 || y >= canvas.height - barSize){
+    context.fillRect( X , canvas.height-barHeight,  barWidth, barHeight )
+    X += directionY
+    if (X <=0 || X >= canvas.width - barWidth){
         directionY *= -1
     }   
     requestAnimationFrame (mainLoop, 60 / 1000)
